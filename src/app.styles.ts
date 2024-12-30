3ba54d8c7968d24e08cf2a85e7e8557cca39f053
JOANNE EMILLY SANTOS DA COSTA <124714961+Joanne-Emilly@users.noreply.github.com>
2024-12-30 13:16:06 -0300

    implementing the footer

--- src/app.styles.ts
+++ src/app.styles.ts
@@ -2,6 +2,40 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   background-color: #111;
+
+  footer {
+    font-size: 3rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    justify-content: center;
+    gap: 1rem;
+    font-family: 'Blinker' sans-serif;
+  }
+
+  footer a {
+    text-decoration: none;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  footer .gitHub {
+    display: flex;
+    align-items: center;
+  }
+
+  footer .tmdb {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  footer .netflix {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
 `;
 
 export default Container;
